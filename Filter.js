9768970763854e34f394bfc0cc64de20fb49f49a
Shanmugam <Ranjithkumar.Shanmugam@trumpf.com>
2026-06-19 17:26:36 +0530

    filter and find element in the array;

---
+++ Filter.js
@@ -0,0 +1,5 @@
+let arr=[1,10,30,5,4,3,9]
+let filteredArray=arr.filter((num)=>{
+    return num%2==0
+})
+console.log(filteredArray);
---
+++ Find.js
@@ -0,0 +1,3 @@
+let arr=[40,20,2,50,30]
+let a=arr.find((num)=> num%2==0 && num>30)
+console.log(a);
