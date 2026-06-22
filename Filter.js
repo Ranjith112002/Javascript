562f9aa0227557743623a08de69c3143cb00188e
Shanmugam <Ranjithkumar.Shanmugam@trumpf.com>
2026-06-22 15:31:07 +0530

    I have modified and improved code reuseability

--- Filter.js
+++ Filter.js
@@ -1,16 +1,8 @@
-
 function getEvenNumbers(numbers) {
-
-    return numbers.filter((number) => {
-
-
-        return number % 2 === 0;
-
-    });
+  return numbers.filter((number) => {
+    return number % 2 === 0;
+  });
 }
-
-const numbers = [1,2,3,4,5,6];
-
+const numbers = [1, 10, 30, 5, 4, 3, 9];
 const evenNumbers = getEvenNumbers(numbers);
-
 console.log(evenNumbers);
