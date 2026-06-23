89aa092f34a0c9a6c8fb6f8e2584f7e1e384d626
Shanmugam <Ranjithkumar.Shanmugam@trumpf.com>
2026-06-23 16:48:25 +0530

    i have modified and improved code reuseability

--- Find.js
+++ Find.js
@@ -1,3 +1,9 @@
-let arr=[40,20,2,50,30]
-let a=arr.find((num)=> num%2==0 && num>30)
-console.log(a);
+function findEvenNumberGreaterThan30(numbers) {
+  return numbers.find((num) => num % 2 === 0 && num > 30);
+}
+
+let arr = [40, 20, 2, 50, 30];
+
+let result = findEvenNumberGreaterThan30(arr);
+
+console.log(result);
