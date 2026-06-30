add751bece4b48bd09cd9182411278a54b24f2d4
Ranjith112002 <Ranjithkumar.Shanmugam@trumpf.com>
2026-06-30 11:24:43 +0530

    Delete Find.js

--- Find.js
+++
@@ -1,6 +0,0 @@
-function findEvenNumberGreaterThan30(numbers) {
-  return numbers.find((num) => num % 2 === 0 && num > 30);
-}
-let arr = [40, 20, 2, 50, 30];
-let result = findEvenNumberGreaterThan30(arr);
-console.log(result);
